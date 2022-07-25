d596ae2f46fecee90a8961f61627ddc6b6875b5a
Stonecracker <62687195+HelmiHasan@users.noreply.github.com>
2022-07-25 07:42:50 -0700

    Time: 12 ms (84.27%), Space: 18.4 MB (36.66%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,7 +1,26 @@
 class Solution {
 public:
+    // function to remove duplicates and return the modified array with new size
     int removeDuplicates(vector<int>& nums) {
-        nums.erase(unique(nums.begin(),nums.end()),nums.end());
-        return nums.size();
+        
+        //  if array is empty or contains single element return this element
+        if(nums.size() <= 1)
+            return nums.size();
+        
+        //  Define two variables (i) iterator (j) to store index of next unique element
+        int i = 0,j = 0;
+        
+        //  Loop for traversing elements
+        for(;i < nums.size()-1; i++)
+            
+        // Check if current element is not equal to next one => then store it
+            if(nums[i] != nums[i+1])
+                nums[j++] = nums[i];
+            
+            
+        // Store last element since, it hasn't stored yet
+         nums[j++] = nums[nums.size()-1];
+        
+        return j;
     }
 };
